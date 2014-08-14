e64686c70d356d1bf2ef6c6a531638dc3f9ec6d3
Aleksandr Zelenin <aleksandr@zelenin.me>
2014-08-15 03:19:40 +0400

    add Alert widget

--- views/default/update.php
+++ views/default/update.php
@@ -10,12 +10,14 @@ use yii\widgets\ActiveForm;
 use yii\widgets\Breadcrumbs;
 use Zelenin\yii\modules\I18n\models\SourceMessage;
 use Zelenin\yii\modules\I18n\Module;
+use Zelenin\yii\widgets\Alert;
 
 $this->title = Module::t('Update') . ': ' . $model->message;
 echo Breadcrumbs::widget(['links' => [
     ['label' => Module::t('Translations'), 'url' => ['index']],
     ['label' => $this->title]
 ]]);
+echo Alert::widget();
 ?>
 <div class="message-update">
     <div class="message-form">

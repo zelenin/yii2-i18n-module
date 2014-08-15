f149104aa5bb8a0a31cf44831babd3ad9311d9ca
Aleksandr Zelenin <aleksandr@zelenin.me>
2014-08-16 00:49:21 +0400

    add ru-RU translation

---
+++ messages/ru-RU/zelenin/modules/i18n.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Category' => 'Категория',
+    'ID' => 'ID',
+    'Language' => 'Язык',
+    'Message' => 'Сообщение',
+    'Source message' => 'Исходное сообщение',
+    'Translation' => 'Перевод',
+    'Translations' => 'Переводы',
+    'Update' => 'Обновить',
+    'Updated' => 'Обновлено',
+];

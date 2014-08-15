30b68c7179a34c51ae59ba4cffba84c6b054956c
Aleksandr Zelenin <aleksandr@zelenin.me>
2014-08-16 00:42:12 +0400

    add export to .php

--- console/controllers/I18nController.php
+++ console/controllers/I18nController.php
@@ -6,6 +6,7 @@ use yii\console\Controller;
 use Yii;
 use yii\console\Exception;
 use yii\helpers\FileHelper;
+use yii\helpers\VarDumper;
 use Zelenin\yii\modules\I18n\models\Message;
 use Zelenin\yii\modules\I18n\models\SourceMessage;
 
@@ -43,6 +44,65 @@ class I18nController extends Controller
             }
         }
         echo PHP_EOL . 'Done.' . PHP_EOL;
+    }
+
+    /**
+     * @param string $messagePath
+     * @param string $category
+     * @throws Exception
+     */
+    public function actionExport($messagePath = null, $category = null)
+    {
+        if (!$messagePath) {
+            $messagePath = $this->prompt('Enter a message path');
+        }
+        $messagePath = realpath(Yii::getAlias($messagePath));
+        if (!is_dir($messagePath)) {
+            throw new Exception('The message path ' . $messagePath . ' is not a valid directory.');
+        }
+
+        if (!$category) {
+            $category = $this->prompt('Enter an exporting category');
+        }
+        if (empty($category)) {
+            throw new Exception('The $category is empty.');
+        }
+
+        $sourceMessages = SourceMessage::find()
+            ->where('category = :category', [':category' => $category])
+            ->orderBy('message')
+            ->all();
+
+        $messages = [];
+
+        foreach ($sourceMessages as $sourceMessage) {
+            $translations = $sourceMessage->messages;
+            foreach (Yii::$app->getI18n()->languages as $language) {
+                $messages[$language][$sourceMessage['message']] = isset($translations[$language]) && !empty($translations[$language]['translation']) ? $translations[$language]['translation'] : '';
+            }
+        }
+
+        foreach ($messages as $language => $translations) {
+            $translationsFile = FileHelper::normalizePath($messagePath . DIRECTORY_SEPARATOR . $language . DIRECTORY_SEPARATOR . str_replace('\\', '/', $category) . '.php');
+            if (!is_file($translationsFile)) {
+                $dir = dirname($translationsFile);
+                if (!FileHelper::createDirectory($dir)) {
+                    throw new Exception('Directory ' . $dir . ' is not created');
+                }
+            }
+            ksort($translations);
+
+            $array = VarDumper::export($translations);
+            $content = <<<EOD
+<?php
+
+return $array;
+
+EOD;
+
+            file_put_contents($translationsFile, $content);
+            echo PHP_EOL . 'Saved to ' . $translationsFile . PHP_EOL;
+        }
     }
 
     /**
